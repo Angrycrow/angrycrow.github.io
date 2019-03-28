7197d0da4c2d70ae045e2fc14f1342b9610320d5
Angrycrow <angrycrow@gmail.com>
2019-03-28 12:35:59 -0700

    Gonna.. Try and get some p5 type things in here..

---
+++ home.js
@@ -0,0 +1,13 @@
+// home 
+// the landing page of this site, not sure what to do with it yet 
+
+function setup()
+{
+    createCanvas(640,480); 
+    
+}
+
+function draw()
+{
+    ellipse(50, 50, 80, 80);
+}
