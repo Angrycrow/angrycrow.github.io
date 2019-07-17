bc4a58bf835a50cfbd637c8a01a8ff38b255d330
Angrycrow <angrycrow@gmail.com>
2019-07-17 15:51:49 -0700

    Added Anon Font.. as the basis for the site.

--- home.js
+++ home.js
@@ -1,12 +1,30 @@
 // home 
 // the landing page of this site, not sure what to do with it yet 
+let plainFont; 
+
+
+function preload()
+{
+    plainFont = loadFont( 'resources/Anonymous Pro.ttf' ); 
+}
+
 
 function setup()
 {
+    // drawing space 
     createCanvas(windowWidth,windowHeight); 
+    
+    // font configuration
+    fill('#FFFFFF');
+    textFont(plainFont);
+    textSize(50);
+    text("Ben Nix-Bradley", windowWidth/2, windowHeight/2); 
+
+
 }
 
 function draw()
 {
     ellipse(mouseX, mouseY, 80, 80);
+    
 }
