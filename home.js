3ec6aeddfc3b9ed75dd9572e60b9d4d82e3bb1bf
Angrycrow <angrycrow@gmail.com>
2021-05-11 14:34:13 -0700

    Cousins!  Everybody is a cousin.

--- home.js
+++ home.js
@@ -1,31 +1,18 @@
 // home 
-// the landing page of this site, not sure what to do with it yet 
-let plainFont; 
-
-
 function preload()
 {
-    plainFont = loadFont( 'resources/Anonymous Pro.ttf' ); 
-}
-
-
-function setup()
-{
-    // drawing space 
-    createCanvas(windowWidth,windowHeight); 
-    
-    // font configuration
-    fill('#556B2F');
-    textFont(plainFont)
-    textSize(50);
-    name = text("Ben Nix-Bradley", 20, 60);
-    status = text("Under Construction", 20, 100); 
-    
-
 
 }
 
-function draw()
-{
-    
-}
+
+function setup() {
+
+   
+  }
+  
+  function draw() {
+
+
+
+  }
+
