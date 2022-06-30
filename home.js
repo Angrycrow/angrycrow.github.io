a7b88a094b66333b173ebff06f5ac3bacb684e89
Ben Nix-Bradley <angrycrow@gmail.com>
2022-06-30 09:35:00 -0700

    Actually starting to branch out with JS..
    Now the fun starts.

--- home.js
+++ home.js
@@ -1,11 +1,3 @@
-const details = document.querySelector("details");
-details.addEventListener("click", function(e) {
-  if (details.hasAttribute("open")) { // since it's not closed yet, it's open!
-    e.preventDefault(); // stop the default behavior, meaning - the hiding
-    details.classList.add("closing"); // add a class which apply the animation in CSS
-    setTimeout(() => { // only after the animation finishes, continue
-      details.removeAttribute("open"); // close the element
-      details.classList.remove("closing");
-    }, 400);
-  }
-});
+function call(){
+    alert("advisory"); 
+}
