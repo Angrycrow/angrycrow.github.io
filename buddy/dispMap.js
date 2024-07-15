e587818634af7e5aa380e7822647610e03acc283
Ben Nix-Bradley <angrycrow@gmail.com>
2024-07-14 18:15:56 -0700

    Finally making inroads. Just need some basic featues. It'll be hard to keep things up to date if I don't think about that.

--- buddy/dispMap.js
+++ buddy/dispMap.js
@@ -1,41 +1,65 @@
+// Basic map display stuff.
 
+
+function initMap() 
+{
+    const map = new google.maps.Map(document.getElementById('map'), 
+    {
+        center: {lat: 34.233, lng: -118.578},
+        zoom: 17,
+        mapId: '9f06e9facff83033',
+        disableDefaultUI: true,
+        zoomControl: true,
+        mapTypeControl: false,
+        scaleControl: true,
+        streetViewControl: true,
+        rotateControl: true,
+        fullscreenControl: false
+    });
+        
     // Creates button in top center. 
     const locationButton = document.createElement("button");
-    infoWindow = new google.maps.InfoWindow();
+    const infoWindow = new google.maps.InfoWindow(); 
     locationButton.textContent = "Pan to Current Location";
     locationButton.classList.add("custom-map-control-button");
+
     map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-    locationButton.addEventListener("click", () => {
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-        (position) => {
-            const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-            };
+    // Scroll to user location. 
+    locationButton.addEventListener("click", () => 
+        {
+            // Try HTML5 geolocation.
+            if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+                };
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent("Location found.");
-            infoWindow.open(map);
-            map.setCenter(pos);
-        },
-        () => {
-            handleLocationError(true, infoWindow, map.getCenter());
-        },
-        );
-        } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-        }
+                infoWindow.setPosition(pos);
+                infoWindow.setContent("Location found.");
+                infoWindow.open(map);
+                map.setCenter(pos);
+            },
+            () => {
+                handleLocationError(true, infoWindow, map.getCenter());
+            },
+            );
+            } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+            }
         });
+}
 
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(
+function handleLocationError(browserHasGeolocation, infoWindow, pos) 
+{
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
         browserHasGeolocation
         ? "Error: The Geolocation service failed."
         : "Error: Your browser doesn't support geolocation.",
-        );
-        infoWindow.open(map);
-    }
+    );
+    infoWindow.open(map);
+}
+
